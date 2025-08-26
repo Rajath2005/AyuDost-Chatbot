5f5ec25eab6498d86e767a1704276d130cbd243a
Rajath Kiran A <arajath5463@gmail.com>
2025-08-26 18:24:03 +0530

    Commit3: Updating the Chatbot Component

--- AyuDost-Chatbot/src/App.jsx
+++ AyuDost-Chatbot/src/App.jsx
@@ -3,6 +3,7 @@ const App = () => {
   return (
     <div class="container">
       <div className="chatbot-popup">
+        {/* Chatbot Header */}
         <div className="chatbot-header">
           <div className="header info">
             <ChatBotIcon />
@@ -10,7 +11,36 @@ const App = () => {
               AyuDost-Chatbot
             </h2>
           </div>
+          <button
+            className="material-symbols-rounded">
+            keyboard_arrow_down</button>
+          {/* Chatbot Body */}
+          <div className="chat body">
+            <div className="message bot-message">
+              <p className="message text">
+                Hey there 👋 <br /> How can i help you today?
+              </p>
+            </div>
+            <div className="message user-message">
+              <p className="message text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse deleniti, quae, totam vitae dolor ut culpa nobis, maxime aliquam sed necessitatibus tempora recusandae? Ab obcaecati enim, sed ad unde exercitationem?
+              </p>
+            </div>
+          </div>
 
+          {/* Chatbot Footer*/}
+
+          <div className="chat footer">
+            <form action="#" className="chat">
+              <input type="text" placeholder="Message..."
+                className="message-inpuut" required />
+              <button
+                className="material-symbols-rounded">
+                arrow_upward</button>
+            </form>
+
+
+          </div>
         </div>
       </div>
 
